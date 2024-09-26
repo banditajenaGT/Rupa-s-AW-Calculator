cc43426a9476eb8982760c4bfbba704f9c3876e7
banditajenaGT <banditajena2001@gmail.com>
2024-09-26 19:14:15 +0530

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ function App() {
     <>
       <div className="flex items-center justify-center h-[100vh] bg-slate-500 overflow-auto max-md:w-[200vw]">
         <div className="flex items-center justify-center bg-slate-700 text-white flex-col border border-blue-300 p-10 rounded-md ">
-          <h1 className=" text-5xl py-3 px-5 font-serif bg-slate-500 rounded-md border border-blue-300">Rupa's Calculator</h1>
+          <h1 className=" text-5xl py-3 px-5 font-serif bg-slate-500 rounded-md border border-blue-300">AW Calculator</h1>
           <table class="table-auto mt-10">
             <thead>
               <tr>
                 <th className="px-3">No. of Children</th>
-                <th className="px-3">Rice (gm)</th>
-                <th className="px-3">Daal (gm)</th>
-                <th className="px-3">Egg (rs)</th>
-                <th className="px-3">Masala (rs)</th>
-                <th className="px-3">Oil (gm)</th>
-                <th className="px-3">Fuel (rs)</th>
-                <th className="px-3">Breakfast (rs)</th>
+                <th className="px-3">Rice (80gm)</th>
+                <th className="px-3">Daal (30gm)</th>
+                <th className="px-3">Egg (5.5rs)</th>
+                <th className="px-3">Masala (0.16rs)</th>
+                <th className="px-3">Oil (3gm)</th>
+                <th className="px-3">Fuel (0.5rs)</th>
+                <th className="px-3">Breakfast (1.24rs)</th>
               </tr>
             </thead>
             <tbody>
@@ -35,7 +35,7 @@ function App() {
                   />
                 </td>
                 <td className="text-center">{80 * children}</td>
-                <td className="text-center">{50 * children}</td>
+                <td className="text-center">{30 * children}</td>
                 <td className="text-center">{5.5 * children}</td>
                 <td className="text-center">{0.16 * children}</td>
                 <td className="text-center">{3 * children}</td>
